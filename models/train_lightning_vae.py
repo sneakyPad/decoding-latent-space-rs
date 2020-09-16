6ecfc2306fa247ed90c4d07f7b9258b546927952
sneakyPad <patrickbszwi@gmail.com>
2020-09-16 16:50:38 +0200

    add match_metadata method

--- models/train_lightning_vae.py
+++ models/train_lightning_vae.py
@@ -54,21 +54,25 @@ from collections import defaultdict
 ##This method creates a user-item matrix by transforming the seen items to 1 and adding unseen items as 0 if simplified_rating is set to True
 ##If set to False, the actual rating is taken
 ##Shape: (n_user, n_items)
-unique_movies = 0
+max_unique_movies = 0
 def create_user_item_matrix(df, simplified_rating: bool):
-    # unique_movies = len(df["movieId"].unique())
-    global unique_movies
-    unique_movies = df["movieId"].unique().max() + 1
+    print('---- Create User Item Matrix ----')
+    global max_unique_movies
+    unique_movies = len(df["movieId"].unique())+1
+    max_unique_movies = df["movieId"].unique().max() + 1
     ls_users = df["userId"].unique()
     unique_users = len(df["userId"].unique()) +1
-    np_user_item_mx = np.zeros((unique_users,unique_movies),dtype=np.float_)
+    np_user_item_mx = np.zeros((unique_users, max_unique_movies), dtype=np.float_)
+    print('Unique movies: {}\nMax unique movies:{}\nUnique users: {}\nShape of Matrix:{}'.format(unique_movies, max_unique_movies, unique_users, np_user_item_mx.shape))
 
+    print('Fill user-item matrix...')
+    print('Stucture of the matrix: \n ______| movie_1 | movie_2 | ... | movie_n \n user_1| \n user_2| \n ... \n user_m|')
     for user_id in ls_users:
-        if(user_id%100 == 0):
-            print("User-Id: {} ".format(user_id))
+        # if(user_id%100 == 0):
+        #     print("User-Id: {} ".format(user_id))
         ls_seen_items = df.loc[df["userId"] == user_id]["movieId"].values
         if(simplified_rating):
-            np_user_item_mx[user_id][ls_seen_items] = 1
+            np_user_item_mx[user_id][ls_seen_items] = 1 #TODO what happens to row 0?
         else:
             #Normalize values
             from sklearn import preprocessing
@@ -80,13 +84,12 @@ def create_user_item_matrix(df, simplified_rating: bool):
             # df_array = ls_rated_items.reshape(-1, 1)
             # ls_normalized_ratings = preprocessing.normalize(df_array, axis=0)
 
-
             # ls_normalized_ratings = normalize(ls_rated_items, norm='max', axis=1)
             # ls_normalized_ratings = min_max_scaler.fit_transform(ls_rated_items)
             ls_normalized_ratings = [float(i) / max(ls_rated_items) for i in ls_rated_items]
             # ls_normalized_ratings = (ls_rated_items - ls_rated_items.min()) / (ls_rated_items.max() - ls_rated_items.min())
             np_user_item_mx[user_id][ls_seen_items] = ls_normalized_ratings
-    return np_user_item_mx.astype(np.float32), unique_movies
+    return np_user_item_mx.astype(np.float32), max_unique_movies
 
 
 class VAE(pl.LightningModule):
@@ -95,22 +98,19 @@ class VAE(pl.LightningModule):
         self.train_dataset = None
         self.test_dataset = None
         self.test_size = kwargs["test_size"]
-        self.unique_movies = 0
+        self.max_unique_movies = 0
         self.np_user_item = None
         self.small_dataset = kwargs["small_dataset"]
         self.simplified_rating = kwargs["simplified_rating"]
 
-
         self.load_dataset() #additionaly assigns self.unique_movies and self.np_user_item
 
-
         #nn.Linear layer creates a linear function (θx + b), with its parameters initialized
-        self.fc1 = nn.Linear(in_features=self.unique_movies, out_features=400) #input
+        self.fc1 = nn.Linear(in_features=self.max_unique_movies, out_features=400) #input
         self.fc21 = nn.Linear(in_features=400, out_features=20) #encoder mean
         self.fc22 = nn.Linear(in_features=400, out_features=20) #encoder variance
         self.fc3 = nn.Linear(in_features=20, out_features=400) #hidden layer
-        self.fc4 = nn.Linear(in_features=400, out_features=self.unique_movies)
-
+        self.fc4 = nn.Linear(in_features=400, out_features=self.max_unique_movies)
 
         # self.save_hyperparameters()
 
@@ -128,7 +128,7 @@ class VAE(pl.LightningModule):
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, self.unique_movies))
+        mu, logvar = self.encode(x.view(-1, self.max_unique_movies))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
@@ -136,16 +136,19 @@ class VAE(pl.LightningModule):
         if (self.small_dataset):
             print("Load small dataset")
             df_ratings = pd.read_csv("../data/movielens/small/ratings.csv")
+
         else:
             print("Load large dataset")
             df_ratings = pd.read_csv("../data/movielens/large/ratings.csv")
 
-        self.np_user_item, self.unique_movies = create_user_item_matrix(df_ratings, simplified_rating=self.simplified_rating)
+        print('Shape of dataset:{}'.format(df_ratings.shape))
+        self.np_user_item, self.max_unique_movies = create_user_item_matrix(df_ratings, simplified_rating=self.simplified_rating)
         self.train_dataset, self.test_dataset = train_test_split(self.np_user_item, test_size=self.test_size, random_state=42)
 
     def train_dataloader(self):
+        #TODO Change shuffle to True, just for dev purpose switched on
         train_loader = torch.utils.data.DataLoader(
-            self.train_dataset, batch_size=128, shuffle=True, num_workers=1, pin_memory=True
+            self.train_dataset, batch_size=128, shuffle=False, num_workers=1, pin_memory=True
         )
         return train_loader
 
@@ -161,14 +164,11 @@ class VAE(pl.LightningModule):
         # scheduler = StepLR(optimizer, step_size=1)
         return optimizer#, scheduler
 
-    # def step(self, ts_batch_user_features):
-
-
     def training_step(self, batch, batch_idx):
         print('train step')
         ts_batch_user_features = batch
         recon_batch, mu, logvar = self.forward(ts_batch_user_features)  # sample data
-        batch_loss = loss_function(recon_batch, ts_batch_user_features, mu, logvar, unique_movies)
+        batch_loss = loss_function(recon_batch, ts_batch_user_features, mu, logvar, max_unique_movies)
         loss = batch_loss/len(ts_batch_user_features)
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
@@ -188,8 +188,8 @@ class VAE(pl.LightningModule):
         #     for i, data in enumerate(test_loader):
 
         recon_batch, mu, logvar = model(ts_batch_user_features)
-        batch_loss = loss_function(recon_batch, ts_batch_user_features, mu, logvar, unique_movies).item()
-        batch_mce = mce_batch(model, ts_batch_user_features)
+        batch_loss = loss_function(recon_batch, ts_batch_user_features, mu, logvar, max_unique_movies).item()
+        batch_mce = mce_batch(model, ts_batch_user_features, k=10)
         loss = batch_loss / len(ts_batch_user_features)
 
         return {'test_loss': batch_loss}
@@ -209,22 +209,28 @@ def load_json_as_dict(name):
         id2names = json.loads(file)
         return id2names
 
-def match_metadata(y_hat, df_links):
-    ls_indezes = y_hat.values.index
+def match_metadata(indezes, df_links):
+    # ls_indezes = y_hat.values.index
     df_links = pd.read_csv('../data/movielens/small/links.csv')
     df_movies = pd.read_csv('../data/movielens/small/df_movies.csv')
-    imdb_ids = df_links.loc[df_links['movieId']==ls_indezes,['imdbId']]
-    y_hat_w_metadata = df_movies.loc[df_movies['ids']==imdb_ids]
-    return y_hat_w_metadata
+    # df_imdb_ids = df_links.loc[df_links['movieId'].isin(indezes),['imdbId']] #dataframe
+    sr_imdb_ids = df_links[df_links["movieId"].isin(indezes)]['imdbId'] #If I want to keep the
+    imdb_ids = sr_imdb_ids.array
+    print('no of imdbIds:{}, no of indezes:{}'.format(len(imdb_ids), len(indezes)))
+    assert len(imdb_ids) == len(indezes)
+    #df_links.loc[df_links["movieId"] == indezes]
+    df_w_metadata = df_movies.loc[df_movies['imdbId'].isin(imdb_ids)]
+
+    return df_w_metadata
 
 
-def mce_relative_frequency():
-    y_hat_w_metadata = match_metadata(y_hat)
+def mce_relative_frequency(y, y_hat):
     dct_dist = load_json_as_dict('relative_frequency.json') #    load relative frequency distributioon from dictionary (pickle it)
     # dct_dist = pickle.load(movies_distribution)
+
     return
 #MCE is calculated for each category
-def mce_batch(model, ts_batch_features):
+def mce_batch(model, ts_batch_features, k):
     # hold n neurons of hidden layer
     # change 1 neuron
     ls_y_hat, mu, logvar = model(ts_batch_features)
@@ -236,6 +242,7 @@ def mce_batch(model, ts_batch_features):
     ls_indizes_not_null = torch.nonzero(ts_batch_features, as_tuple=False)
 
 
+    #Collect the index of the items that were seen
     ls_non_zeroes = torch.nonzero(ts_batch_features, as_tuple=True)
     tpl_users = ls_non_zeroes[0]
     tpl_items = ls_non_zeroes[1]
@@ -244,18 +251,24 @@ def mce_batch(model, ts_batch_features):
         user_idx = int(tpl_users[idx])
         item_idx = int(tpl_items[idx])
         dct_seen_items[user_idx].append(item_idx)
-        print(dct_seen_items)
+        # print(dct_seen_items)
 
     #TODO Can be slow, find another way
-    for user_idx, item_idx in ls_indizes_not_null:
-        mask_not_null[user_idx][item_idx] = True
+    # for user_idx, item_idx in ls_indizes_not_null:
+    #     mask_not_null[user_idx][item_idx] = True
     # mask_not_null = ts_batch_features > 0 TODO This is an alternative to the for loop, test it
 
+
     #Go through the list of list of the predicted batch
-    for y_hat in ls_y_hat:
+    for user_idx, ls_seen_items in dct_seen_items.items():
+        y_hat = ls_y_hat[user_idx]
+        no_of_seen_items = len(ls_seen_items) #TODO Add k
+        y_hat_k_highest = (-y_hat).argsort()[:no_of_seen_items] #Alternative: (-y_hat).sort().indices[:no_of_seen_items]
+
+        # y_hat_w_metadata = match_metadata(y_hat_k_highest.tolist(), df_links)
+        y_w_metadata = match_metadata(ls_seen_items, df_links)
 
 
-        y_hat_w_metadata = match_metadata(y_hat, df_links)
 
     # for attribute in y_hat_w_metadata:
     #     characteristic = y_hat_w_metadata[attribute].value
@@ -263,13 +276,9 @@ def mce_batch(model, ts_batch_features):
     #     print('Attribute: {}, Relative frequency:{}'.format(attribute, relative_frequency))
 
 
-
-
-
-
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar, unique_movies):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, unique_movies), reduction='sum')
+def loss_function(recon_x, x, mu, logvar, max_unique_movies):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, max_unique_movies), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -293,62 +302,6 @@ def loss_function(recon_x, x, mu, logvar, unique_movies):
 
 train_dataset = None
 test_dataset = None
-# def train(dct_hyperparam: dict, simplified_rating: bool, small_dataset: bool):
-#
-#     model = VAE(input_shape=unique_movies).to(device)  # load it to the specified device, either gpu or cpu
-#     # optimizer = optim.Adam(model.parameters(), lr= dct_hyperparam['learning_rate']) # create an optimizer object -Adam optimizer with learning rate 1e-3
-#
-#
-#
-#     ## VAE IMPORTED ---
-#     model.train()
-#     for epoch in range(dct_hyperparam["epochs"]):
-#         loss = 0
-#         y_hat = []
-#         batch_idx = 0
-#         train_loss = 0
-#
-#         for batch_idx, ts_batch_user_features in enumerate(train_loader):
-#             ts_batch_user_features = ts_batch_user_features.to(device)
-#             optimizer.zero_grad()
-#
-#             recon_batch, mu, logvar = model(ts_batch_user_features) #sample data
-#             loss = loss_function(recon_batch, ts_batch_user_features, mu, logvar, unique_movies)
-#
-#
-#             loss.backward()
-#             batch_loss = loss.item()
-#             loss = batch_loss / len(ts_batch_user_features)
-#             train_loss += loss
-#             optimizer.step()
-#             if batch_idx % args.log_interval == 0:
-#                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-#                     epoch, batch_idx * len(ts_batch_user_features), len(train_loader.dataset),
-#                            100. * batch_idx / len(train_loader),
-#                     loss))
-#
-#             # avg_epoch_loss = train_loss / len(train_loader) Smooth, but weird numbers
-#         avg_epoch_loss = train_loss / batch_idx
-#
-#         writer.add_scalar('training loss', avg_epoch_loss, epoch)
-#         print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, avg_epoch_loss))
-
-    ## VAE END IMPORTED ---
-
-
-# def test():
-#     model.eval()
-#     test_loss = 0
-#     with torch.no_grad():
-#         for i, data in enumerate(test_loader):
-#             data = data.to(device)
-#             recon_batch, mu, logvar = model(data)
-#             test_loss += loss_function(recon_batch, data, mu, logvar, unique_movies).item()
-#
-#
-#     test_loss /= len(test_loader.dataset)
-#     print('====> Test set loss: {:.4f}'.format(test_loss))
-
 #%%
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
@@ -389,33 +342,35 @@ import recmetrics
 #%%
 model_params = {"simplified_rating": True,
                 "small_dataset": True,
-                "test_size": 0.33}
+                "test_size": 0.33} #TODO Change test size to 0.33
 # model_params.update(args.__dict__)
 # print(**model_params)
 
 merged_params = (lambda first_dict, second_dict: {**first_dict, **second_dict})(args.__dict__, model_params)
-print(merged_params)
+# print(merged_params)
 
 
 #%%
 
 
-neptune_logger = NeptuneLogger(
-    #api_key="key",
-    project_name="paer/recommender-xai",
-    experiment_name="neptune-logs",  # Optional,
-    params = merged_params,
-    # params={"max_epochs": 1,
-    #         "batch_size": 32},  # Optional,
-    close_after_fit=True, #must be False if test method should be logged
-    tags=["pytorch-lightning", "vae"]  # Optional,
-
-)
+# neptune_logger = NeptuneLogger(
+#     #api_key="key",
+#     project_name="paer/recommender-xai",
+#     experiment_name="neptune-logs",  # Optional,
+#     params = merged_params,
+#     # params={"max_epochs": 1,
+#     #         "batch_size": 32},  # Optional,
+#     close_after_fit=True, #must be False if test method should be logged
+#     tags=["pytorch-lightning", "vae","unique-movies-large"]  # Optional,
+#
+# )
 
 trainer = pl.Trainer.from_argparse_args(args,
                                         #max_epochs=1, automatically processed by Pytorch Light
-                                        logger=neptune_logger,
-                                        gpus=0,
+                                        #logger=neptune_logger,
+                                        logger=False,
+                                        checkpoint_callback = False,
+                                        gpus=0
                                         #num_nodes=4
 )
 
@@ -425,11 +380,14 @@ trainer = pl.Trainer.from_argparse_args(args,
 #%%
 from torchsummaryX import summary
 import hiddenlayer as hl
+print('<---------------------------------- VAE Training ---------------------------------->')
 print("Running with the following configuration: \n{}".format(args))
 model = VAE(**model_params)
-print(model)
+# print(model)
 # summary(model, (193610))
+print('------ Start Training ------')
 trainer.fit(model)
+print('------ Start Test ------')
 trainer.test(model) #The test loop will not be used until you call.
 
 #%%
