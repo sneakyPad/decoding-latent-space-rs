b706f8ca56e0a0e6685b4e5df1df42a41eec1471
sneakyPad <patrickbszwi@gmail.com>
2020-10-22 21:25:34 +0200

    move calculation of relative frequency to utils and add model for ae

--- data retrieval/metadata_handler.py
+++ data retrieval/metadata_handler.py
@@ -18,6 +18,7 @@ from time import sleep
 import math
 import json
 import janitor
+import utils.utils as utils
 
 # manager = multiprocessing.Manager()
 # shared_list = manager.list()
@@ -395,66 +396,6 @@ def crawl_metadata(ls_imdb_ids,multi_processing, no_processes, develop_size):
     print('Shape of crawled dataset:{}'.format(df_meta.shape[0]))
     return df_meta
 
-def my_eval(expression):
-    try:
-        return ast.literal_eval(str(expression))
-    except SyntaxError: #e.g. a ":" or "(", which is interpreted by eval as command
-            return expression
-    except ValueError: #e.g. an entry is nan, in that case just return an empty string
-        return ''
-
-def compute_relative_frequency(df_meta):
-    print('Compute relative frequency for all columns and their attributes...')
-    #Goal is:
-    #Cast:
-        # Tom Hanks: 0,3%
-        # Matt Damon: 0,2%
-    # fpp =np.vstack(df_meta['genres'].values)
-    # np_array = df_meta['genres'].values
-    # tmp_list = []
-    # for element in np_array:
-    #     tmp_list.extend(eval(element))
-
-    # print(fpp)len_crawled_ids
-    #TODO Implement my eval: https://stackoverflow.com/questions/31423864/check-if-string-can-be-evaluated-with-eval-in-python
-    dct_rel_freq={}
-    for column in tqdm(df_meta.columns, total=len(df_meta.columns)):
-        print('Column: {}'.format(column))
-    #     ls_ls_casted=[]
-    #     for str_elem in df_meta[column].values:
-    #         str_elem= str(str_elem)#.replace(':','').replace('(','').replace(')','')
-    #         try:
-    #             ls_ls_casted.append(ast.literal_eval(str_elem))
-    #         except SyntaxError:
-    #             ls_ls_casted.append(str_elem)
-
-        ls_ls_casted = [my_eval(str_elem) for str_elem in df_meta[column].values] #cast encoded lists to real list
-        # ls_ls_casted = [json.loads(str(str_elem)) for str_elem in df_meta[column].values] #cast encoded lists to real list
-        try:
-            if(type(ls_ls_casted[0]) == list):
-                merged_res = itertools.chain(*ls_ls_casted) #join all lists to one single list
-                ls_merged = list(merged_res)
-            else:
-                ls_merged = ls_ls_casted
-            if(column not in ['Unnamed: 0', 'unnamed_0']):
-                c = Counter(ls_merged)
-                dct_counter = {str(key): value for key, value in c.items()}
-                dct_rel_freq[column]={}
-                dct_rel_freq[column]['absolute'] = dct_counter
-                # print('Column: {}\n\t absolute:{}'.format(dct_rel_freq[column]['absolute']))
-
-                dct_rel_attribute = {str(key): value / sum(c.values()) for key, value in dct_counter.items()} #TODO create a dict with key val
-                dct_rel_freq[column]['relative'] = dct_rel_attribute
-                # print('\t relative:{}'.format(dct_rel_freq[column]['relative']))
-
-        except TypeError:
-            print('TypeError for Column:{} and ls_ls_casted:{} and *ls_ls_casted:{}'.format(column, ls_ls_casted, *ls_ls_casted))
-
-
-    return dct_rel_freq
-    # save_dict_as_json(dct_rel_freq, 'relative_frequency.json')
-
-
 
         # tmp_list = []
         # for element in df_meta[column]:
@@ -550,7 +491,7 @@ def main():
     for col in df_meta.columns:
         if(col not in df_links_joined_one.columns):
             df_links_joined_one[col]=""
-    #Extend origiinial dataframe by columns of new one:
+    #Extend originial dataframe by columns of new one:
     for row_idx in range(df_meta.shape[0]):
         row = df_meta.loc[row_idx,]
         row['imdbId'] = int(row['imdbId'])
@@ -577,7 +518,7 @@ def main():
     # transform names to ids
     df_meta = names2ids(df=df_meta, column_name=['cast', 'stars'])
 
-    dct_attribute_distribution = compute_relative_frequency(df_meta)
+    dct_attribute_distribution = utils.compute_relative_frequency(df_meta)
     # Save data
     print('Save enhanced movielens dataset...')
     if (small_dataset):
@@ -619,7 +560,7 @@ if __name__ == '__main__':
     # df.to_csv('../data/generated/df_movies_cleaned3.csv', index=False)
 
 
-    dct_attribute_distribution = compute_relative_frequency(pd.read_csv('../data/generated/df_movies_cleaned3.csv'))
+    dct_attribute_distribution = utils.compute_relative_frequency(pd.read_csv('../data/generated/df_movies_cleaned3.csv'))
     save_dict_as_json(dct_attribute_distribution, 'attribute_distribution.json')
 
     #%%
